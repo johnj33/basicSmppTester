fb198cb06fda7586f7237cb885fd62a5cb037bae
John Jackson <john.jackson@esendex.com>
2018-06-19 16:22:41 +0100

    Add basic smpp tester

---
+++ SMPPTester/SMPPTester/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+
+namespace SMPPTester
+{
+    class Program
+    {
+        static void Main()
+        {
+            var smppHelper = new SmppHelper();
+            var count = int.Parse(ConfigurationManager.AppSettings["numberOfMessagesToSend"]);
+            var orginator = ConfigurationManager.AppSettings["originator"];
+            var recipient = ConfigurationManager.AppSettings["recipient"];
+            var message = ConfigurationManager.AppSettings["message"];
+
+            var submits = new List<string>();
+            for (var i = 0; i <= count; i++)
+            {
+                smppHelper.Send(orginator, recipient, message);
+                var submissionResponse = smppHelper.WaitForSubmissionResponse(TimeSpan.FromMinutes(1));
+                var submissionResponseMessageId = submissionResponse.MessageId;
+
+                submits.Add(submissionResponseMessageId);
+                Console.WriteLine($"sent: {submissionResponseMessageId}");
+            }
+
+            var counter = 0;
+
+            foreach (var submit in submits)
+            {
+                var deliverSm =
+                    smppHelper.WaitForDeliveryReceipt(TimeSpan.FromMinutes(1), submit);
+                Console.WriteLine($"messageNo : {counter++}; status: {deliverSm.MessageState}");
+            }
+            Console.WriteLine("finished");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SMPPTester/SMPPTester/SmppHelper.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Configuration;
+using AberrantSMPP;
+using AberrantSMPP.Packet;
+using AberrantSMPP.Packet.Request;
+using AberrantSMPP.Packet.Response;
+using AberrantSMPP.Utility;
+
+namespace SMPPTester
+{
+    public class SmppHelper : IDisposable
+    {
+        private readonly SMPPCommunicator _client;
+
+        private readonly BlockingCollection<SmppSubmitSmResp> _submissionResponses = new BlockingCollection<SmppSubmitSmResp>();
+        private readonly BlockingCollection<SmppDeliverSm> _deliveryReceipts = new BlockingCollection<SmppDeliverSm>();
+        private readonly BlockingCollection<SmppDeliverSm> _inboundMessages = new BlockingCollection<SmppDeliverSm>();
+
+        public SmppHelper()
+        {
+            _client = new SMPPCommunicator
+                      {
+                          Host = ConfigurationManager.AppSettings["hostName"],
+                          Port = Convert.ToUInt16(ConfigurationManager.AppSettings["port"]),
+                          SystemId = ConfigurationManager.AppSettings["username"],
+                          Password = ConfigurationManager.AppSettings["password"],
+                          BindType = SmppBind.BindingType.BindAsTransceiver,
+                          Version = Pdu.SmppVersionType.Version3_4
+                      };
+
+            _client.OnSubmitSmResp += (source, args) =>
+            {
+                _submissionResponses.Add(args.ResponsePdu);
+            };
+
+            _client.OnDeliverSm += (source, args) =>
+            {
+                _client.SendPdu(new SmppDeliverSmResp()
+                {
+                    CommandStatus = CommandStatus.ESME_ROK,
+                    SequenceNumber = args.DeliverSmPdu.SequenceNumber,
+                });
+
+                if ((args.DeliverSmPdu.EsmClass & 0x04) == 0x4)
+                    _deliveryReceipts.Add(args.DeliverSmPdu);
+                else
+                    _inboundMessages.Add(args.DeliverSmPdu);
+            };
+
+            _client.Bind();
+        }
+
+        public void Send(string orginator, string recipient, string body)
+        {
+            var submit = new SmppSubmitSm
+            {
+                DataCoding = DataCoding.SMSCDefault,
+                DestinationAddress = recipient,
+                DestinationAddressNpi = Pdu.NpiType.ISDN,
+                DestinationAddressTon = Pdu.TonType.International,
+                SourceAddress = orginator,
+                SourceAddressNpi = Pdu.NpiType.Unknown,
+                SourceAddressTon = Pdu.TonType.Alphanumeric,
+                RegisteredDelivery = Pdu.RegisteredDeliveryType.OnSuccessOrFailure,
+                ShortMessage = new GSMEncoding().GetBytes(body)
+            };
+
+            _client.SendPdu(submit);
+        }
+
+        public SmppSubmitSmResp WaitForSubmissionResponse(TimeSpan timeout)
+        {
+            if (_submissionResponses.TryTake(out var submissionResponse, timeout)) return submissionResponse;
+
+            throw new Exception($"Did not receive Submission Response within {timeout}");
+        }
+
+        public SmppDeliverSm WaitForDeliveryReceipt(TimeSpan timeout, string id)
+        {
+            var timeoutTime = DateTime.UtcNow + timeout;
+
+            while (timeoutTime > DateTime.UtcNow)
+            {
+                if (_deliveryReceipts.TryTake(out var deliveryReceipt, timeoutTime - DateTime.UtcNow) &&
+                    deliveryReceipt.ReceiptedMessageId == id) return deliveryReceipt;
+            }
+
+            throw new Exception($"Did not receive Delivery Receipt within {timeout}");
+
+        }
+
+        public SmppDeliverSm WaitForInboundMessage(TimeSpan timeout, string body)
+        {
+            var timeoutTime = DateTime.UtcNow + timeout;
+
+            while (timeoutTime > DateTime.UtcNow)
+            {
+                if (_inboundMessages.TryTake(out var inboundMessage, timeoutTime - DateTime.UtcNow) &&
+                    inboundMessage.ShortMessage == body) return inboundMessage;
+
+            }
+            throw new Exception($"Did not receive Inbound Message within {timeout}");
+        }
+
+        public void Dispose()
+        {
+            _client.Dispose();
+        }
+    }
+}
